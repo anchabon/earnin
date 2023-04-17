b78ec594872ca13cfd929d324482f3ce7714c6ac
Anchabon (Frong) Laohakunvanich <s.france1998@gmail.com>
2023-04-18 02:23:35 +0700

    change to earnin1

--- E2E/earnin.spec.ts
+++ E2E/earnin.spec.ts
@@ -1,14 +1,23 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, devices } from '@playwright/test';
+
+test.use({
+  ...devices['iPhone 13'],
+});
 
 test('test', async ({ page }) => {
+  await test.setTimeout(120000);
   await page.goto('https://news.sky.com/');
+//   await page.waitForLoadState('networkidle');
   await page.frameLocator('iframe[title="SP Consent Message"]').getByRole('button', { name: 'Accept all' }).click();
-  await page.getByTestId('site-header-more-nav-button').click();
-  await page.getByTestId('site-header-more-nav').getByText('Weather').click();
+  await page.getByTestId('site-header-hamburger').click();
+  await page.getByRole('link', { name: 'Weather' }).click();
   await page.getByRole('link', { name: 'London' }).click();
   await page.getByPlaceholder('Type a city name').fill('manchester');
   await page.getByPlaceholder('Type a city name').press('Enter');
-  await page.getByRole('link', { name: 'Manchester, Manchester, United Kingdom' }).click();
-//   await page.getByText('5pm').click();
-//   await page.getByText('9°C').nth(2).click();
+  await page.getByRole('link', { name: 'Manchester, Manchester, United Kingdom' }).click();  
+//   await page.getByRole('link', { name: 'Manchester, Lancashire, United Kingdom' }).click();  
+//   await page.waitForLoadState('networkidle');
+//   await expect(page).toHaveScreenshot('weather.png');
+  await expect(page).toHaveScreenshot('weather.png',{maxDiffPixelRatio:0.00,clip: {x: 0,y: 0,width: 390,height: 670}});
+
 });
